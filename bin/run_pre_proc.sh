c01b305d657255cd8975e097f9215c444d27f53c
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-03-01 18:45:29 +0000

    Paths corrected for JASMIN

--- bin/run_pre_proc.sh
+++ bin/run_pre_proc.sh
@@ -2,14 +2,14 @@
 # run_pre_proc.sh
 # A wrapper around run_pre_proc.py to set the appropriate environment
 # variables.
-INSTALL_DIR=/home/h04/jseddon/primavera/pre-proc
+INSTALL_DIR=/home/users/jseddon/primavera/pre-proc
 
-source /home/h04/jseddon/primavera/venvs/django/bin/activate
+source $INSTALL_DIR/../venvs/django/bin/activate
 export DJANGO_SETTINGS_MODULE=pre_proc_site.settings
 export PYTHONPATH=.
 
 
-export DATABASE_DIR=`mktemp -d --suffix=prima-crepp.XXXXXXX`
+export DATABASE_DIR=`mktemp -d /tmp/prima-crepp.XXXXXXX`
 cp $INSTALL_DIR/pre_proc_site/db/pre-proc_db.sqlite3 $DATABASE_DIR
 
 python2.7 $INSTALL_DIR/bin/run_pre_proc.py -l debug "$@"
