2f2c6df68463e410db34b79958570bc8831803dd
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-04-18 13:24:54 +0100

    Several AWI variables working.

--- bin/fix_requests/fix_request_0006.py
+++ bin/fix_requests/fix_request_0006.py
@@ -4,7 +4,7 @@ fix_request_0006.py
 
 Fix some AWI data. Convert the further_info_url attribute on all AWI data
 from HTTP to HTTPS.
-Fix additional items for variable Omon/so.
+Fix additional items for variable Omon/so and Omon/tos.
 """
 import argparse
 import logging.config
@@ -71,6 +71,15 @@ def main():
         cmor_name='so'
     )
 
+    data_reqs_tos = DataRequest.objects.filter(
+        institution_id__name='AWI',
+        experiment_id__name__in=['spinup-1950', 'hist-1950', 'control-1950'],
+        table_id='Omon',
+        cmor_name='tos'
+    )
+
+    data_reqs_common = data_reqs_so.union(data_reqs_tos)
+
     parent_branch_time = FileFix.objects.get(name='ParentBranchTimeDoubleFix')
     child_branch_time = FileFix.objects.get(name='ChildBranchTimeDoubleFix')
     r_fix = FileFix.objects.get(name='RealizationIndexIntFix')
@@ -79,15 +88,18 @@ def main():
     f_fix = FileFix.objects.get(name='ForcingIndexIntFix')
     parent_source_id = FileFix.objects.get(name='ParentSourceIdFromSourceId')
     salinity = FileFix.objects.get(name='SeaWaterSalinityStandardNameAdd')
+    sea_temp = FileFix.objects.get(name='SeaSurfaceTemperatureNameAdd')
     cell_methdods = FileFix.objects.get(name='CellMethodsSeaAreaTimeMeanAdd')
-    cell_measures = FileFix.objects.get(name='CellMeasuresAreacelloVolcelloAdd')
+    cell_measures_areavol = FileFix.objects.get(name='CellMeasuresAreacelloVolcelloAdd')
+    cell_measures_area = FileFix.objects.get(name='CellMeasuresAreacelloAdd')
     var_units = FileFix.objects.get(name='VarUnitsToThousandths')
+    fill_value = FileFix.objects.get(name='FillValueFromMissingValue')
 
     # This next line could be done more quickly by:
     # further_info_url_fix.datarequest_set.add(*data_reqs)
     # but sqlite3 gives an error of:
     # django.db.utils.OperationalError: too many SQL variables
-    for data_req in data_reqs_so:
+    for data_req in data_reqs_common:
         data_req.fixes.add(parent_branch_time)
         data_req.fixes.add(child_branch_time)
         data_req.fixes.add(r_fix)
@@ -95,33 +107,48 @@ def main():
         data_req.fixes.add(p_fix)
         data_req.fixes.add(f_fix)
         data_req.fixes.add(parent_source_id)
-        data_req.fixes.add(salinity)
         data_req.fixes.add(cell_methdods)
-        data_req.fixes.add(cell_measures)
+
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(parent_branch_time.name, data_reqs_common.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(child_branch_time.name, data_reqs_common.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(r_fix.name, data_reqs_common.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(i_fix.name, data_reqs_common.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(p_fix.name, data_reqs_common.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(f_fix.name, data_reqs_common.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(parent_source_id.name, data_reqs_common.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(cell_methdods.name, data_reqs_common.count()))
+
+    for data_req in data_reqs_so:
+        data_req.fixes.add(salinity)
+        data_req.fixes.add(cell_measures_areavol)
         data_req.fixes.add(var_units)
 
     logger.debug('FileFix {} added to {} data requests.'.
-                 format(parent_branch_time.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
-                 format(child_branch_time.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
-                 format(r_fix.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
-                 format(i_fix.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
-                 format(p_fix.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
-                 format(f_fix.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
-                 format(parent_source_id.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
                  format(salinity.name, data_reqs_so.count()))
     logger.debug('FileFix {} added to {} data requests.'.
-                 format(cell_methdods.name, data_reqs_so.count()))
-    logger.debug('FileFix {} added to {} data requests.'.
-                 format(cell_measures.name, data_reqs_so.count()))
+                 format(cell_measures_areavol.name, data_reqs_so.count()))
     logger.debug('FileFix {} added to {} data requests.'.
                  format(var_units.name, data_reqs_so.count()))
+
+    for data_req in data_reqs_tos:
+        data_req.fixes.add(sea_temp)
+        data_req.fixes.add(cell_measures_area)
+        data_req.fixes.add(fill_value)
+
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(sea_temp.name, data_reqs_tos.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(cell_measures_area.name, data_reqs_tos.count()))
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(fill_value.name, data_reqs_tos.count()))
 
 
 if __name__ == "__main__":
