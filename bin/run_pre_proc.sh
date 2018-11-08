81b2bdb2ad5749cdb8cf0b58ec2e26d290191d27
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-11-08 17:37:40 +0000

    Call Python from within conda

--- bin/run_pre_proc.sh
+++ bin/run_pre_proc.sh
@@ -3,8 +3,10 @@
 # A wrapper around run_pre_proc.py to set the appropriate environment
 # variables.
 INSTALL_DIR=/home/users/jseddon/primavera/pre-proc
+CONDA_DIR=/group_workspaces/jasmin2/primavera1/tools/miniconda2/bin
+CONDA_ENV_DIR=/group_workspaces/jasmin2/primavera1/tools/miniconda2/envs/py3-6/bin
 
-export PATH=/group_workspaces/jasmin2/primavera1/tools/miniconda2/bin:$PATH
+export PATH=$CONDA_DIR:$PATH
 . activate py3-6
 export DJANGO_SETTINGS_MODULE=pre_proc_site.settings
 export PYTHONPATH=$INSTALL_DIR
@@ -12,7 +14,7 @@ export PYTHONPATH=$INSTALL_DIR
 export DATABASE_DIR=`mktemp -d /tmp/prima-crepp.XXXXXXX`
 cp $INSTALL_DIR/db/pre-proc_db.sqlite3 $DATABASE_DIR
 
-python $INSTALL_DIR/bin/run_pre_proc.py -l debug "$@"
+$CONDA_ENV_DIR/python $INSTALL_DIR/bin/run_pre_proc.py -l debug "$@"
 RETURN_CODE=$?
 
 rm $DATABASE_DIR/pre-proc_db.sqlite3
