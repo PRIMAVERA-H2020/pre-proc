46d3d99dd727785840aa08d19fd7dded9b6c23af
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-11-08 13:54:27 +0000

    Import ABCs for add_file_fixes_to_db.py

--- pre_proc/file_fix/__init__.py
+++ pre_proc/file_fix/__init__.py
@@ -3,6 +3,7 @@ file_fix/__init__.py
 
 The workers that fix the netCDF files
 """
+from .abstract import *
 from .attribute_update import *
 from .copy_attribute import *
 from .attribute_add import *
