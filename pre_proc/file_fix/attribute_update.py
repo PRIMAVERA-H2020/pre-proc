0612e33f7b5412aa1b92137ccab9b7c219210783
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-12-24 15:06:59 +0000

    PrePARE now passes

--- pre_proc/file_fix/attribute_update.py
+++ pre_proc/file_fix/attribute_update.py
@@ -251,3 +251,30 @@ class FurtherInfoUrlAWISourceIdAndHttps(AttributeUpdate):
 
         if self.source_id is None:
             raise AttributeNotFoundError(self.filename, 'source_id')
+
+
+class AogcmToAgcm(AttributeUpdate):
+    """
+    Change the global attribute `source_type` from `AOGCM` to `AGCM`.
+    """
+    def __init__(self, filename, directory):
+        """
+        Initialise the class
+
+        :param str filename: The basename of the file to process.
+        :param str directory: The directory that the file is currently in.
+        """
+        super(AogcmToAgcm, self).__init__(filename, directory)
+        self.attribute_name = 'source_type'
+        self.attribute_visibility = 'global'
+        self.attribute_type = 'c'
+
+    def _calculate_new_value(self):
+        """
+        Check the existing attribute is `AOGCM` and change it to `AGCM`.
+        """
+        if self.existing_value != 'AOGCM':
+            raise ExistingAttributeError(self.filename, self.attribute_name,
+                                           'Existing value is not AOGCM.')
+        else:
+            self.new_value = 'AGCM'
