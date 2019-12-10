6f10ed2d265c1571228d9d40273f962076995e3e
Jon Seddon <jon.seddon@metoffice.gov.uk>
2019-12-10 17:26:22 +0000

    Docstring and PEP8

--- bin/make_primavera_byte_masks.sh
+++ bin/make_primavera_byte_masks.sh
@@ -2,7 +2,8 @@
 # make_primavera_byte_masks.sh
 #
 # Convert the ocean byte mask files to netCDF3 and rename the dimensions so
-# that they match the data files
+# that they match the data files. netCDF3 is required because the netCDF4
+# library sometimes struggles to rename dimensions.
 
 byte_mask_dir="/gws/nopw/j04/primavera1/masks/HadGEM3Ocean_fixes/bytes_masks"
 declare -a resolutions=("HadGEM3-GC31-LL" "HadGEM3-GC31-MM")
@@ -18,5 +19,3 @@ do
     ncrename -d x,i -d y,j -d deptht,lev "$res_dir/$temp_file" "$res_dir/$output_file";
     rm "$res_dir/$temp_file";
 done
-
-
