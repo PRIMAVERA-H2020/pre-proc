5c80d45daca79f9f6833bc5944ad5ff85ceac8bb
Jon Seddon <jon.seddon@metoffice.gov.uk>
2020-01-29 13:57:02 +0000

    MOHC ice ORCA data to areacello

---
+++ bin/fix_requests/fix_request_6415.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+"""
+fix_request_6415.py
+
+MOHC.ice_ORCA.*
+
+In all MOHC ice data on ORCA grids fix change cell_measures to be areacello.
+"""
+import argparse
+import logging.config
+import sys
+
+import django
+django.setup()
+
+from pre_proc_app.models import DataRequest, FileFix
+
+
+__version__ = '0.1.0b1'
+
+DEFAULT_LOG_LEVEL = logging.WARNING
+DEFAULT_LOG_FORMAT = '%(levelname)s: %(message)s'
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    """
+    Parse command-line arguments
+    """
+    parser = argparse.ArgumentParser(description='Add pre-processing rules.')
+    parser.add_argument('-l', '--log-level', help='set logging level to one '
+                                                  'of debug, info, warn (the '
+                                                  'default), or error')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s {}'.format(__version__))
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    """
+    Main entry point
+    """
+    simon = DataRequest.objects.filter(
+        source_id__name='HadGEM3-GC31-LL',
+        table_id='SImon',
+        cmor_name__in=[
+            'sitimefrac', 'simass', 'sithick', 'sivol', 'sisnconc', 'sisnmass',
+            'sisnthick', 'sitempbot', 'siage', 'sihc', 'sisnhc', 'siflfwdrain',
+            'sidmassth', 'sidmassdyn', 'sidmassmelttop', 'sidmassmeltbot',
+            'sifllatstop', 'siflsensupbot', 'siflcondtop', 'siflcondbot',
+            'sipr', 'siflsaltbot', 'siflfwbot', 'siu', 'siv', 'sispeed',
+            'sistrxdtop', 'sistrydtop', 'sistrxubot', 'sistryubot', 'sidivvel'
+        ]
+    )
+
+    siday = DataRequest.objects.filter(
+        source_id__name='HadGEM3-GC31-LL',
+        table_id='SIday',
+        cmor_name__in=['sithick', 'siu', 'siv']
+    )
+
+    primsiday = DataRequest.objects.filter(
+        source_id__name='HadGEM3-GC31-LL',
+        table_id='SIday',
+        cmor_name__in=[
+            'sitimefrac', 'sistrxdtop', 'sistrydtop', 'sistrxubot',
+            'sistryubot', 'siforceintstrx', 'siforceintstry', 'sidivvel'
+        ]
+    )
+
+    data_reqs = (simon | siday | primsiday)
+
+    fixes = [
+        FileFix.objects.get(name='CellMeasuresAreacelloAdd'),
+    ]
+
+    # This next line could be done more quickly by:
+    # further_info_url_fix.datarequest_set.add(*data_reqs)
+    # but sqlite3 gives an error of:
+    # django.db.utils.OperationalError: too many SQL variables
+    for data_req in data_reqs:
+        data_req.fixes.add(*fixes)
+
+    num_data_reqs = data_reqs.count()
+    for fix in fixes:
+        logger.debug('FileFix {} added to {} data requests.'.
+                     format(fix.name, num_data_reqs))
+
+
+if __name__ == "__main__":
+    cmd_args = parse_args()
+
+    # determine the log level
+    if cmd_args.log_level:
+        try:
+            log_level = getattr(logging, cmd_args.log_level.upper())
+        except AttributeError:
+            logger.setLevel(logging.WARNING)
+            logger.error('log-level must be one of: debug, info, warn or error')
+            sys.exit(1)
+    else:
+        log_level = DEFAULT_LOG_LEVEL
+
+    # configure the logger
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': DEFAULT_LOG_FORMAT,
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': log_level,
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': log_level,
+                'propagate': True
+            }
+        }
+    })
+
+    # run the code
+    main()
