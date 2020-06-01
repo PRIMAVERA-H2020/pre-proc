c41c0660fa31f8f74434fd713ee4f6ec226d0c23
Jon Seddon <jon.seddon@metoffice.gov.uk>
2020-06-01 12:59:37 +0100

    Apply PRIMAVERA further_info_url fix for HadGEM[F

---
+++ bin/fix_requests/fix_request_6004.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+"""
+fix_request_6004.py
+
+MOHC.*.Prim*
+
+Convert the further_info_url attribute on all MOHC and NERC data from HTTP to
+HTTPS and update the activity_id.
+"""
+import argparse
+import logging.config
+import sys
+
+import django
+django.setup()
+
+from pre_proc_app.models import DataRequest, FileFix
+
+
+__version__ = '0.1.0b1'
+
+DEFAULT_LOG_LEVEL = logging.WARNING
+DEFAULT_LOG_FORMAT = '%(levelname)s: %(message)s'
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    """
+    Parse command-line arguments
+    """
+    parser = argparse.ArgumentParser(description='Add pre-processing rules.')
+    parser.add_argument('-l', '--log-level', help='set logging level to one '
+                                                  'of debug, info, warn (the '
+                                                  'default), or error')
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s {}'.format(__version__))
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    """
+    Main entry point
+    """
+    data_reqs = DataRequest.objects.filter(
+        institution_id__name__in=['MOHC', 'NERC'],
+        table_id__startswith='Prim'
+    )
+
+    # Remove existing CMIP6 fix
+    further_info_url_fix = FileFix.objects.get(name='FurtherInfoUrlToHttps')
+
+    # This next line could be done more quickly by:
+    # further_info_url_fix.datarequest_set.add(*data_reqs)
+    # but sqlite3 gives an error of:
+    # django.db.utils.OperationalError: too many SQL variables
+    for data_req in data_reqs:
+        data_req.fixes.remove(further_info_url_fix)
+
+    logger.debug('FileFix {} removed from {} data requests.'.
+                 format(further_info_url_fix.name, data_reqs.count()))
+
+    # Add PRIMAVERA fix
+    prim_further_info_fix = FileFix.objects.get(
+        name='FurtherInfoUrlPrimToHttps'
+    )
+
+    for data_req in data_reqs:
+        data_req.fixes.add(prim_further_info_fix)
+
+    logger.debug('FileFix {} added to {} data requests.'.
+                 format(prim_further_info_fix.name, data_reqs.count()))
+
+
+if __name__ == "__main__":
+    cmd_args = parse_args()
+
+    # determine the log level
+    if cmd_args.log_level:
+        try:
+            log_level = getattr(logging, cmd_args.log_level.upper())
+        except AttributeError:
+            logger.setLevel(logging.WARNING)
+            logger.error('log-level must be one of: debug, info, warn or error')
+            sys.exit(1)
+    else:
+        log_level = DEFAULT_LOG_LEVEL
+
+    # configure the logger
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': DEFAULT_LOG_FORMAT,
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': log_level,
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': log_level,
+                'propagate': True
+            }
+        }
+    })
+
+    # run the code
+    main()
