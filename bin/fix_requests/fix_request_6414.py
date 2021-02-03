3330afad3292633bd961f32a46cf4c6a35885642
Jon Seddon <jon.seddon@metoffice.gov.uk>
2021-02-03 16:39:38 +0000

    Fix mask on ORCA1

--- bin/fix_requests/fix_request_6414.py
+++ bin/fix_requests/fix_request_6414.py
@@ -67,6 +67,7 @@ def main():
 
     fixes = [
         FileFix.objects.get(name='FixCiceCoords1UV'),
+        FileFix.objects.get(name='FixMaskCICEOrca1UV'),
     ]
 
     # This next line could be done more quickly by:
