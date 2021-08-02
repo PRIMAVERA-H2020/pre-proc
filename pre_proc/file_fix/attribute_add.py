4657a9c73045404ecb845e30a82fc2b78c93e91c
Jon Seddon <jon.seddon@metoffice.gov.uk>
2021-08-02 15:25:51 +0100

    grid_label gn

--- pre_proc/file_fix/attribute_add.py
+++ pre_proc/file_fix/attribute_add.py
@@ -922,6 +922,31 @@ class GeopotentialHeightNameAdd(AttributeAdd):
         self.new_value = 'geopotential_height'
 
 
+class GridLabelGnAdd(AttributeAdd):
+    """
+    Add a global attribute `grid_label` with a value of
+    `gn`. This is done in overwrite mode and so will work
+    irrespective of whether there is an existing standard_name attribute.
+    """
+    def __init__(self, filename, directory):
+        """
+        Initialise the class
+
+        :param str filename: The basename of the file to process.
+        :param str directory: The directory that the file is currently in.
+        """
+        super().__init__(filename, directory)
+        self.attribute_name = 'grid_label'
+        self.attribute_visibility = 'global'
+        self.attribute_type = 'c'
+
+    def _calculate_new_value(self):
+        """
+        Set the new value.
+        """
+        self.new_value = 'gn'
+
+
 class HadGemMMParentSourceId(AttributeAdd):
     """
     Add a global attribute `parent_source_id` with a value for HadGEM3-GC31-MM.
