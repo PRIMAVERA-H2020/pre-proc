93fe50916d6e1c1ab1e1984dd8b3ea4b0ef24295
Jon Seddon <jon.seddon@metoffice.gov.uk>
2021-08-04 10:39:03 +0100

    cell_methods:: area: sum where sea time: mean

--- pre_proc/file_fix/attribute_add.py
+++ pre_proc/file_fix/attribute_add.py
@@ -621,6 +621,32 @@ class CellMethodsAreaMeanTimeMaxDailyAdd(AttributeAdd):
         self.new_value = 'area: mean time: maximum'
 
 
+class CellMethodsAreaSumSeaTimeMeanAdd(AttributeAdd):
+    """
+    Add a variable attribute `cell_methods` with a value of
+    `area: sum where sea time: mean`. This is done
+    in overwrite mode and so will work irrespective of whether there is an
+    existing cell_methods attribute.
+    """
+    def __init__(self, filename, directory):
+        """
+        Initialise the class
+
+        :param str filename: The basename of the file to process.
+        :param str directory: The directory that the file is currently in.
+        """
+        super().__init__(filename, directory)
+        self.attribute_name = 'cell_methods'
+        self.attribute_visibility = self.variable_name
+        self.attribute_type = 'c'
+
+    def _calculate_new_value(self):
+        """
+        Set the new value.
+        """
+        self.new_value = 'area: sum where sea time: mean'
+
+
 class EcEarthInstitution(AttributeAdd):
     """
     Add a global attribute `institution` with a value for EC-Earth. This
